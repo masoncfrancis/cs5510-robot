723136761abcef12a035128020ce216388f1649f
Caleb Jeppson <caleb.jeppson@gmail.com>
2023-09-28 17:47:04 -0600

    changed listen.py to allow for more natural movement options

--- listen.py
+++ listen.py
@@ -4,55 +4,113 @@ from pynput.keyboard import Key
 from Car import Car
 import time
 
+#usage: first value indicates forward (+) or backward (-), second indicates left (-) or right (+)
+#purpose: more natural driving - forward and backward cancel each other, as do left and right, and releasing one key doesn't simply stop the whole car from any and all action. Additionally, allows for simultaneous turning and moving
+drive_vector = [0, 0]
+
+speed = 100
+
+def drive():
+    if drive_vector[0] > 0:
+        if drive_vector[1] == 0:
+            #directly forward
+            car.control_car(speed, speed)
+        elif drive_vector[1] > 0:
+            #forward and right
+            car.control_car(speed, 0)
+        else:
+            #forward and left
+            car.control_car(0, speed)
+    elif drive_vector[0] < 0:
+        if drive_vector[1] == 0:
+            #directly backward
+            car.control_car(-speed, -speed)
+        elif drive_vector[1] > 0:
+            #backward and right
+            car.control_car(-speed, 0)
+        else:
+            #backward and left
+            car.control_car(0, -speed)
+    else:
+        if drive_vector[1] == 0:
+            #no motion
+            car.control_car(0, 0)
+        elif drive_vector[1] > 0:
+            #only right
+            car.control_car(speed, -speed)
+        else:
+            #only left
+            car.control_car(-speed, speed)
+
 
 def press_callback(key):
     try:
         # print('alphanumeric key {} pressed'.format(key.char))
         if key.char == 'w':
-            print('moving forward')
-            # car.control_car(100, 100)
+            #forward
+            drive_vector[0] += 1
         elif key.char == 'a':
-            print('turning left')
-            # car.control_car(-100, 100)
+            #left
+            drive_vector[1] -= 1
         elif key.char == 's':
-            print('moving backward')
-            # car.control_car(-100, -100)
+            #backward
+            drive_vector[0] -= 1
         elif key.char == 'd':
-            print('turning right')
-            # car.control_car(100, -100)
+            #right
+            drive_vector[1] += 1
         else:
             print("invalid action")
-            # car.control_car(0, 0)
     except AttributeError:
-        # print('special key {} pressed'.format(key))
         if key == Key.up:
-            print('moving forward')
-            # car.control_car(100, 100)
+            #forward
+            drive_vector[0] += 1
         elif key == Key.down:
-            print('moving backward')
-            # car.control_car(-100, -100)
+            #backward
+            drive_vector[0] -= 1
         elif key == Key.right:
-            print('turning right')
-            # car.control_car(100, -100)
+            #right
+            drive_vector[1] += 1
         elif key == Key.left:
-            print('turning left')
-            # car.control_car(-100, 100)
+            #left
+            drive_vector[1] -= 1
         elif key == Key.esc:
+            #stop the car
+            drive_vector = [0, 0]
             print('quiting...')
         else:
             print("invalid action")
-            # car.control_car(0, 0)
+    drive()
 
 def release_callback(key):
     # print('{} released'.format(key))
     try:
         if key.char in ['w','a','s','d']:
-            print('stopped moving/turning')
-            # car.control_car(0, 0)
+            if key.char == 'w':
+            #stop forward
+            drive_vector[0] -= 1
+        elif key.char == 'a':
+            #stop left
+            drive_vector[1] += 1
+        elif key.char == 's':
+            #stop backward
+            drive_vector[0] += 1
+        elif key.char == 'd':
+            #stop right
+            drive_vector[1] -= 1
     except AttributeError:
-        if key in [Key.up,Key.left,Key.down,Key.right]:
-            print('stopped moving/turning')
-            # car.control_car(0, 0)
+        if key == Key.up:
+            #stop forward
+            drive_vector[0] -= 1
+        elif key == Key.down:
+            #stop backward
+            drive_vector[0] += 1
+        elif key == Key.right:
+            #stop right
+            drive_vector[1] -= 1
+        elif key == Key.left:
+            #stop left
+            drive_vector[1] += 1
+    drive()
     if key == Key.esc:
         return False
 
