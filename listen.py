35718fe41dc7150793edafe7892c752785944985
Caleb Jeppson <caleb.jeppson@gmail.com>
2023-09-29 10:51:38 -0600

    wrapped callback functions in a class to allow for persistent data

--- listen.py
+++ listen.py
@@ -5,90 +5,92 @@ from sshkeyboard import listen_keyboard
 from Car import Car
 import time
 
-#usage: first value indicates forward (+) or backward (-), second indicates left (-) or right (+)
-#purpose: more natural driving - forward and backward cancel each other, as do left and right, and releasing one key doesn't simply stop the whole car from any and all action. Additionally, allows for simultaneous turning and moving
-drive_vector = [0, 0]
+class keyTracker:
+    #usage: first value indicates forward (+) or backward (-), second indicates left (-) or right (+)
+    #purpose: more natural driving - forward and backward cancel each other, as do left and right, and releasing one key doesn't simply stop the whole car from any and all action. Additionally, allows for simultaneous turning and moving
+    drive_vector = [0, 0]
 
-# braking = False
+    braking = False
 
-speed = 100
+    speed = 100
 
-def drive():
-    if drive_vector[0] > 0:
-        if drive_vector[1] == 0:
-            #directly forward
-            car.control_car(speed, speed)
-        elif drive_vector[1] > 0:
-            #forward and right
-            car.control_car(speed, 0)
+    def drive(self):
+        if self.drive_vector[0] > 0:
+            if self.drive_vector[1] == 0:
+                #directly forward
+                car.control_car(speed, speed)
+            elif self.drive_vector[1] > 0:
+                #forward and right
+                car.control_car(speed, 0)
+            else:
+                #forward and left
+                car.control_car(0, speed)
+        elif self.drive_vector[0] < 0:
+            if self.drive_vector[1] == 0:
+                #directly backward
+                car.control_car(-speed, -speed)
+            elif self.drive_vector[1] > 0:
+                #backward and right
+                car.control_car(-speed, 0)
+            else:
+                #backward and left
+                car.control_car(0, -speed)
         else:
-            #forward and left
-            car.control_car(0, speed)
-    elif drive_vector[0] < 0:
-        if drive_vector[1] == 0:
-            #directly backward
-            car.control_car(-speed, -speed)
-        elif drive_vector[1] > 0:
-            #backward and right
-            car.control_car(-speed, 0)
-        else:
-            #backward and left
-            car.control_car(0, -speed)
-    else:
-        if drive_vector[1] == 0:
-            #no motion
-            car.control_car(0, 0)
-        elif drive_vector[1] > 0:
-            #only right
-            car.control_car(speed, -speed)
-        else:
-            #only left
-            car.control_car(-speed, speed)
+            if self.drive_vector[1] == 0:
+                #no motion
+                car.control_car(0, 0)
+            elif self.drive_vector[1] > 0:
+                #only right
+                car.control_car(speed, -speed)
+            else:
+                #only left
+                car.control_car(-speed, speed)
 
 
-def press_callback(key):
-    # if braking:
-        # return
-    
-    if key == 'w' or key == 'up':
-        #forward
-        drive_vector[0] += 1
-    elif key == 'a' or key == 'left':
-        #left
-        drive_vector[1] -= 1
-    elif key == 's'or key == 'down':
-        #backward
-        drive_vector[0] -= 1
-    elif key == 'd'or key == 'right':
-        #right
-        drive_vector[1] += 1
-    elif key == 'space':
-        #force stop
-        braking = True
-        drive_vector = [0,0]
-    drive()
+    def press_callback(self, key):
+        if self.braking:
+            return
+        
+        if key == 'w' or key == 'up':
+            #forward
+            self.drive_vector[0] += 1
+        elif key == 'a' or key == 'left':
+            #left
+            self.drive_vector[1] -= 1
+        elif key == 's'or key == 'down':
+            #backward
+            self.drive_vector[0] -= 1
+        elif key == 'd'or key == 'right':
+            #right
+            self.drive_vector[1] += 1
+        elif key == 'space':
+            #force stop
+            self.braking = True
+            self.drive_vector = [0,0]
+        self.drive()
 
-def release_callback(key):
-    # if key == 'space':
-        # braking = False
-    # elif braking:
-        # return
-    
-    if key == 'w' or key == 'up':
-        #stop forward
-        drive_vector[0] -= 1
-    elif key == 'a'or key == 'left':
-        #stop left
-        drive_vector[1] += 1
-    elif key == 's'or key == 'down':
-        #stop backward
-        drive_vector[0] += 1
-    elif key == 'd' or key == 'right':
-        #stop right
-        drive_vector[1] -= 1
-    drive()
+    def release_callback(self, key):
+        if key == 'space':
+            self.braking = False
+        elif self.braking:
+            return
+        
+        if key == 'w' or key == 'up':
+            #stop forward
+            self.drive_vector[0] -= 1
+        elif key == 'a'or key == 'left':
+            #stop left
+            self.drive_vector[1] += 1
+        elif key == 's'or key == 'down':
+            #stop backward
+            self.drive_vector[0] += 1
+        elif key == 'd' or key == 'right':
+            #stop right
+            self.drive_vector[1] -= 1
+        drive()
 
 # l = keyboard.Listener(on_press=press_callback, on_release=release_callback)
-listen_keyboard(on_press=press_callback,on_release=release_callback)
+k = keyTracker()
+listen_keyboard(on_press=k.press_callback,on_release=k.release_callback)
 # l.start()
 # l.join()
