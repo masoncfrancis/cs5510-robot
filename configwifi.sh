f5a29a9a6233b13e5806253f5a02a68e8b1ab58b
Mason Francis <a02244655@usu.edu>
2023-10-11 10:06:16 -0600

    Added wifi config file, still needs testing

---
+++ configwifi.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Check if SSID is provided as an argument
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <SSID> [password]"
+    exit 1
+fi
+
+# Set SSID and password variables
+SSID=$1
+PASSWORD=$2
+
+# Create the YAML configuration
+if [ -z "$PASSWORD" ]; then
+    cat <<EOL > /etc/netplan/50-cloud-init.yaml
+network:
+  version: 2
+  renderer: networkd
+  wifis:
+    wlan0:
+      dhcp4: yes
+      optional: true
+      access-points:
+        "$SSID": {}
+EOL
+else
+    cat <<EOL > /etc/netplan/50-cloud-init.yaml
+network:
+  version: 2
+  renderer: networkd
+  wifis:
+    wlan0:
+      dhcp4: yes
+      optional: true
+      access-points:
+        "$SSID":
+          password: "$PASSWORD"
+EOL
+fi
+
+# Apply the new configuration
+netplan apply
