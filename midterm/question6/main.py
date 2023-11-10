1e1870aee9978b4119f15ed30db3dd70ba0f0401
Mason <masoncfrancis@gmail.com>
2023-11-09 21:32:06 -0700

    Separated code into question part files. Added videos

--- midterm/question6/main.py
+++ midterm/question6/main.py
@@ -1,8 +1,25 @@
 from fer import FER
 import cv2
+from fer.utils import draw_annotations
 
-img = cv2.imread("images/test.jpg")
+
+# Resources to learn how to write this code came from FER documentation and examples
+
 detector = FER()
-result = detector.detect_emotions(img)
+capture = cv2.VideoCapture(0)
 
-print(result)
+while True:
+    ret, frame = capture.read()
+
+    frame = cv2.flip(frame, 1)
+
+    emotions = detector.detect_emotions(frame)
+    frame = draw_annotations(frame, emotions)
+
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
+
